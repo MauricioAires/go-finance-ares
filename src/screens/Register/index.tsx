157f83314badb6d56ada8b6d8681902504b5808a
MauricioAires <mauricioaires015@gmail.com>
2024-01-29 21:36:15 -0300

    feat(core) base register page

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,22 @@
+import { Button } from "../../components/Forms/Button";
+import { Input } from "../../components/Forms/Input";
+import * as S from "./styles";
+
+export function Register() {
+  return (
+    <S.Container>
+      <S.Header>
+        <S.Title>Cadastro</S.Title>
+      </S.Header>
+
+      <S.Form>
+        <S.Fields>
+          <Input placeholder="Nome" />
+          <Input placeholder="Preço" />
+        </S.Fields>
+
+        <Button title="Enviar" />
+      </S.Form>
+    </S.Container>
+  );
+}
